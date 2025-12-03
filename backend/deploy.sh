bd77acac03840fca9021dd27cbd64f5ecb48c57d
Carlos A Palacios R <carlosargelio0104@gmail.com>
2025-12-03 19:34:37 -0400

    cd: test deploy

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -22,7 +22,7 @@ ls -al src/
 
 pwd
 
-python src/manager.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); \
+python manager.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); \
     username = os.environ.get('SUPERUSER_USERNAME', 'admin'); \
     email = os.environ.get('SUPERUSER_EMAIL', 'admin@example.com'); \
     password = os.environ.get('SUPERUSER_PASSWORD', 'secret'); \
